d7f38432934035ddcaf443848d4025a7a473c862
Manel2313 <djemal.manel@gmail.com>
2019-05-08 10:12:27 +0100

    Updates

---
+++ precache-manifest.62e0a3a9f9aacc459cce17001fdedba2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5706540bcc5fef86d0ec506379191f",
+    "url": "/Static-Timer/index.html"
+  },
+  {
+    "revision": "bfc0107bf58da736d2a9",
+    "url": "/Static-Timer/static/css/main.b6f79c08.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Static-Timer/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bfc0107bf58da736d2a9",
+    "url": "/Static-Timer/static/js/main.f1c867cd.chunk.js"
+  },
+  {
+    "revision": "2bc4b1226f6fe85388a6",
+    "url": "/Static-Timer/static/js/runtime~main.eb7899a6.js"
+  }
+]);
